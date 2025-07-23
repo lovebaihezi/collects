ba48213bfc5854f8efd05d40fb48e8319d2dcc7a
BoWen Chai <lovebaihezi@gmail.com>
2025-07-23 18:01:52 +0800

    ci: deploy to cloudflare pages

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "Collects",
         native_options,
-        Box::new(|cc| Ok(Box::new(collects_app::TemplateApp::new(cc)))),
-    )
+                Box::new(|cc| Ok(Box::new(collects::TemplateApp::new(cc)))),    )
 }
 
 // When compiling to web using trunk:
@@ -50,8 +49,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(collects_app::TemplateApp::new(cc)))),
-            )
+        Box::new(|cc| Ok(Box::new(collects::TemplateApp::new(cc)))),            )
             .await;
 
         // Remove the loading text and spinner:

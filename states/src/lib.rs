d0ab6d6271f5350d8f6170b5265cae6d53b76e0f
BoWen Chai <lovebaihezi@gmail.com>
2025-11-21 20:56:10 +0800

    feat: how to use the state 2/N

--- states/src/lib.rs
+++ states/src/lib.rs
@@ -11,7 +11,7 @@ pub use basic_states::BasicStates;
 pub use compute::Compute;
 pub use ctx::StateCtx;
 pub use graph::{DepRoute, Graph, TopologyError};
-pub use register_state::register_state;
+pub use register_state::Reg;
 pub use runtime::StateRuntime;
 pub use state::{State, StateReader, StateUpdater};
 pub use state_sync_status::StateSyncStatus;
@@ -19,33 +19,4 @@ pub use state_sync_status::StateSyncStatus;
 #[cfg(test)]
 mod state_runtime_test {
     use super::*;
-
-    #[test]
-    fn simple_state() {
-        let mut runtime = StateRuntime::new();
-        let sender = runtime.sender();
-        let receiver = runtime.receiver();
-
-        struct TestState {
-            value: i32,
-        }
-
-        impl Default for TestState {
-            fn default() -> Self {
-                Self { value: 0 }
-            }
-        }
-
-        impl State for TestState {
-            const TYPE: &'static str = "test_state";
-            const ID: Reg = Reg::BasicStates; // Just an example, use appropriate Reg
-        }
-
-        let test_state = TestState { value: 42 };
-        updater.set(test_state);
-
-        let boxed = receiver.recv().unwrap();
-        let state = boxed.downcast::<TestState>().unwrap();
-        assert_eq!(state.value, 42);
-    }
 }

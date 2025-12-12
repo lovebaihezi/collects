0d6f85b5feb8887019ce76bf5d28b40dc9c54ddc
BoWen Chai <lovebaihezi@gmail.com>
2025-12-12 17:07:33 +0800

    fixup

--- business/src/env.rs
+++ business/src/env.rs
@@ -1,13 +1,69 @@
-use ustr::Ustr;
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum AppType {
+pub enum EnvType {
     Prod,
     Test,
     Local,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AppType {
+    Web,
+    Native,
+}
+
 pub struct AppEnv {
+    env_type: EnvType,
     app_type: AppType,
-    worker_url: Ustr,
+}
+
+impl AppEnv {
+    pub fn native_prod() -> Self {
+        Self {
+            env_type: EnvType::Prod,
+            app_type: AppType::Native,
+        }
+    }
+
+    pub fn native_test() -> Self {
+        Self {
+            env_type: EnvType::Test,
+            app_type: AppType::Native,
+        }
+    }
+
+    pub fn native_local() -> Self {
+        Self {
+            env_type: EnvType::Local,
+            app_type: AppType::Native,
+        }
+    }
+
+    pub fn web_prod() -> Self {
+        Self {
+            env_type: EnvType::Prod,
+            app_type: AppType::Web,
+        }
+    }
+
+    pub fn web_test() -> Self {
+        Self {
+            env_type: EnvType::Test,
+            app_type: AppType::Web,
+        }
+    }
+
+    pub fn web_local() -> Self {
+        Self {
+            env_type: EnvType::Local,
+            app_type: AppType::Web,
+        }
+    }
+
+    pub fn env_type(&self) -> EnvType {
+        self.env_type
+    }
+
+    pub fn app_type(&self) -> AppType {
+        self.app_type
+    }
 }
